78093a38ba796981c3611df2a7112dcc9685e96f
Jordi Henrique Silva <jordi.silva@ufv.br>
2020-12-04 15:57:53 -0300

    first commit

---
+++ src/main/java/digital/innovationone/personapi/controller/personController.java
@@ -0,0 +1,14 @@
+package digital.innovationone.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class personController {
+    @GetMapping
+    public String book(){
+        return "Deu certo!";
+    }
+}
